b501508beba0400f24af4747294776c1e50ea1d9
farrukh0913 <farrukh0913@gmail.com>
2024-04-29 19:39:40 +0500

    async and await

--- index.js
+++ index.js
@@ -29,18 +29,25 @@ app.use((req, res, next) => {
 const port = 3000;
 // routes(app);
 const url = `mongodb+srv://wwwburjsoft:gBLTbWrZsDYS9kR5@cluster0.k2bwmju.mongodb.net/?retryWrites=true&w=majority`;
-mongoose
-  .connect(url)
-  .then(() => {
-    console.log("Connected to database!", url);
-  })
-  .catch((error) => {
-    console.log("Connection failed!:", error);
-  });
+
+const connect = async () => {
+  try {
+      await mongoose.connect(url).then(() => {
+        console.log("Connected to database!", url);
+      }).catch((error) => {
+        console.log("Connection failed!:", error);
+      });
+  } catch(error) {
+      console.log('Problemas al conectarse a la DB. Error ->', error);
+  }
+};
+
+
+connect();
 
 server.listen(port, () => {
   console.log(`Express server listening ${port}`);
   app.get('/', function (req, res) {
-    res.send('**** Hello World! APP is running faster2. ****');
+    res.send('**** Hello World! APP is running faster3. ****');
   });
 });
