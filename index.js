b7b58124a7bd46d5f65f34839730be253104ffe9
farrukh0913 <farrukh0913@gmail.com>
2024-04-29 19:42:11 +0500

    change4

--- index.js
+++ index.js
@@ -31,17 +31,13 @@ const port = 3000;
 const url = `mongodb+srv://wwwburjsoft:gBLTbWrZsDYS9kR5@cluster0.k2bwmju.mongodb.net/?retryWrites=true&w=majority`;
 
 const connect = async () => {
-  try {
       await mongoose.connect(url).then(() => {
         console.log("Connected to database!", url);
       }).catch((error) => {
         console.log("Connection failed!:", error);
       });
-  } catch(error) {
-      console.log('Problemas al conectarse a la DB. Error ->', error);
   }
 };
-
 
 connect();
 
