2f6a3f39cc0101715dab97d3650ee81778a09746
farrukh0913 <farrukh0913@gmail.com>
2024-04-29 20:35:43 +0500

    15

--- index.js
+++ index.js
@@ -6,14 +6,16 @@ const port = 3000;
 // routes(app);
 const url = `mongodb+srv://wwwburjsoft:gBLTbWrZsDYS9kR5@cluster0.k2bwmju.mongodb.net/?retryWrites=true&w=majority`;
 
-mongoose
-  .connect(url)
-  .then(() => {
-    console.log("Connected to database!", url);
-  })
-  .catch((error) => {
-    console.log("Connection failed!:", error);
-  });
+// const conn = mongoose.createConnection(url);
+// mongoose.connect(url);
+// mongoose
+//   .connect(url)
+//   .then(() => {
+//     console.log("Connected to database!", url);
+//   })
+//   .catch((error) => {
+//     console.log("Connection failed!:", error);
+//   });
 
   app.listen(port, () => {
   console.log(`Express server listening ${port}`);
