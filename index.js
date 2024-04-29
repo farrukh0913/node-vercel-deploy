df473045de42c4a0bbf763f6dabaa9d0815b691a
farrukh0913 <farrukh0913@gmail.com>
2024-04-29 19:57:07 +0500

    10

--- index.js
+++ index.js
@@ -7,45 +7,22 @@ const cors = require("cors");
 const app = express();
 const server = http.createServer(app);
 
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
-app.use(bodyParser.json({ limit: "50mb" }));
-app.use(  cors({ origin: "*"}));
-
-app.use((req, res, next) => {
-  res.setHeader("Content-Type", "application/json");
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,PUT, POST, PATCH, DELETE, OPTIONS"
-  );
-  next();
-});
-
 const port = 3000;
 // routes(app);
 const url = `mongodb+srv://wwwburjsoft:gBLTbWrZsDYS9kR5@cluster0.k2bwmju.mongodb.net/?retryWrites=true&w=majority`;
-
-// mongoose.connect(
-//   url
-// ).catch((e) => {
-//   console.log("error connecting to mongoose!");
-// });
-// mongoose.connection.on("error", (e) => {
-//   console.log("mongo connect error!");
-// });
-// mongoose.connection.on("connected", () => {
-//   console.log("connected to mongo");
-// });
-
+mongoose
+  .connect(url)
+  .then(() => {
+    console.log("Connected to database!", url);
+  })
+  .catch((error) => {
+    console.log("Connection failed!:", error);
+  });
 
 server.listen(port, () => {
   console.log(`Express server listening ${port}`);
-  app.get('/', function (req, res) {
-    res.send('**** Hello World! APP is running faster3. ****');
-  });
+});
+
+app.get('/', function (req, res) {
+  res.send('**** Hello World! APP is running faster2. ****');
 });
